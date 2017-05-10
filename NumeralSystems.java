9eba543992d252966b305b5297314d9c34eb02f0
Eyad AL-‘Amawi <ealamawi@smail.ucas.edu.ps>
2017-05-10 12:11:05 +0300

    Update NumeralSystems.java

--- NumeralSystems.java
+++ NumeralSystems.java
@@ -4,6 +4,7 @@ package NumberSystems;
  * This program convert the same integer from any one of the main systems
  * (Binary,Octal,Decimal and Hexadecimal) to any one of these systems
  * and mathematical operations (addition, subtraction, multiplication, division). 
+ *
  * @author Eyad AL-‘Amawi
  */
 public class NumeralSystems {
